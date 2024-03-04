b08eef30932ac07fda4b2755042ea5f60791db8f
Ali Hamed <alihamed.cg@gmail.com>
2024-03-04 19:35:02 +0200

    finish the get users method in the user controller

---
+++ Code/NewsWebsiteBackEnd/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewsWebsiteBackEnd.Models;
+using NewsWebsiteBackEnd.Models.ViewModels.Pagination;
+using NewsWebsiteBackEnd.Models.ViewModels.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewsWebsiteBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUsers> _userManager;
+
+        public UserController(UserManager<ApplicationUsers> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllUsers([FromQuery] PaginationModel pagination)
+        {
+            try
+            {
+                var usersQuery = _userManager.Users
+                    .Where(u => !u.IsDeleted) 
+                    .Select(u => new UserDetailsViewModel
+                    {
+                        Id = u.Id,
+                        UserName = u.UserName,
+                        Email = u.Email,
+                        FullName = u.FullName,
+                        UserTypeId = u.UserTypeId,
+                        UserTypeName = u.UserType.Name 
+                    });
+
+                if (pagination.EndRow.HasValue)
+                {
+                    usersQuery = usersQuery.Skip(pagination.StartRow).Take(pagination.EndRow.Value - pagination.StartRow);
+                }
+
+                var users = await usersQuery.ToListAsync();
+
+                return Ok(new { success = true, message = "Done.", data = users });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { success = false, message = $"Exception Error: {ex.Message}." });
+            }
+        }
+
+    }
+}
---
+++ Code/NewsWebsiteBackEnd/Models/ViewModels/User/UserDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsWebsiteBackEnd.Models.ViewModels.User
+{
+    public class UserDetailsViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeName { get; set; }
+    }
+}
