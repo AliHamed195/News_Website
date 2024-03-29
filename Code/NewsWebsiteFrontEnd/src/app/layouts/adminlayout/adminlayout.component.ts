8ea70cd5c6c3afe34c051b70e37713a0e3b01ce5
Ali Hamed <alihamed.cg@gmail.com>
2024-03-29 23:47:07 +0200

    finish the admin nave bar and side bar

--- Code/NewsWebsiteFrontEnd/src/app/layouts/adminlayout/adminlayout.component.ts
+++ Code/NewsWebsiteFrontEnd/src/app/layouts/adminlayout/adminlayout.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateCategoriesComponent } from '../../pages/category/create-categories/create-categories.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-adminlayout',
   standalone: true,
-  imports: [RouterOutlet,CreateCategoriesComponent],
+  imports: [
+    RouterOutlet,
+    CreateCategoriesComponent,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: './adminlayout.component.html',
-  styleUrl: './adminlayout.component.css'
+  styleUrl: './adminlayout.component.css',
 })
-export class AdminlayoutComponent {
-
-}
+export class AdminlayoutComponent {}
