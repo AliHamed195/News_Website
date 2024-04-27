d708ded1bb489ca2f672a08a5c9b72b289d0b732
Ali Hamed <alihamed.cg@gmail.com>
2024-04-27 19:29:29 +0300

    finish  the read articl for the home page

--- Code/NewsWebsiteFrontEnd/src/app/components/adds/adds.component.ts
+++ Code/NewsWebsiteFrontEnd/src/app/components/adds/adds.component.ts
@@ -1,7 +1,6 @@
 import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
+
 @Component({
   selector: 'app-adds',
   standalone: true,
@@ -10,28 +9,5 @@ import { takeWhile } from 'rxjs/operators';
   styleUrl: './adds.component.css',
 })
 export class AddsComponent {
-  @Input() ads: string[] = [];
-  @Input() intervalSec = 100;
-
-  currentAdIndex = 0;
-  addSecPath: string | undefined;
-  private adsSubscription: Subscription | undefined;
-
-  ngOnInit() {
-    this.addSecPath = this.ads[this.currentAdIndex];
-    const adsLength = this.ads.length;
-
-    this.adsSubscription = interval(this.intervalSec * 1000)
-      .pipe(takeWhile(() => this.ads.length > 0))
-      .subscribe(() => {
-        this.currentAdIndex = (this.currentAdIndex + 1) % adsLength;
-        this.addSecPath = this.ads[this.currentAdIndex];
-      });
-  }
-
-  ngOnDestroy() {
-    if (this.adsSubscription) {
-      this.adsSubscription.unsubscribe();
-    }
-  }
+  @Input() addSecPath: string | undefined;
 }
--- Code/NewsWebsiteFrontEnd/src/app/pages/home-article-details-page/home-article-details-page.component.ts
+++ Code/NewsWebsiteFrontEnd/src/app/pages/home-article-details-page/home-article-details-page.component.ts
@@ -1,21 +1,39 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { PaginationModel } from './../../models/pagination/pagination-model';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticlesService } from '../../Services/Articles/articles.service';
 import { ArticleDetailsViewModel } from '../../models/article/article-details-view-model';
 import { ArticleForReadComponent } from '../../components/article-for-read/article-for-read.component';
-import { NgIf, isPlatformBrowser } from '@angular/common';
+import { NgFor, NgIf, isPlatformBrowser } from '@angular/common';
 import { AddsComponent } from '../../components/adds/adds.component';
+import { MedioumArticleCardComponent } from '../../components/medioum-article-card/medioum-article-card.component';
+import { GeneralArticleDetailsViewModel } from '../../models/article/general-article-details-view-model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-article-details-page',
   standalone: true,
-  imports: [NgIf, ArticleForReadComponent, AddsComponent],
+  imports: [
+    NgIf,
+    NgFor,
+    ArticleForReadComponent,
+    AddsComponent,
+    MedioumArticleCardComponent,
+  ],
   templateUrl: './home-article-details-page.component.html',
   styleUrl: './home-article-details-page.component.css',
 })
-export class HomeArticleDetailsPageComponent implements OnInit {
+export class HomeArticleDetailsPageComponent implements OnInit, OnDestroy {
   articleUrl: string | null = null;
-  article: ArticleDetailsViewModel | null = null;
+  thearticle: ArticleDetailsViewModel | null = null;
+  someArticles: GeneralArticleDetailsViewModel[] = [];
+  private paramMapSubscription: Subscription | undefined;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -25,21 +43,49 @@ export class HomeArticleDetailsPageComponent implements OnInit {
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.route.paramMap.subscribe((params) => {
-        this.articleUrl = params.get('url');
-        if (this.articleUrl) {
-          this.loadArticleDetails(this.articleUrl);
-        }
+      this.paramMapSubscription = this.route.paramMap.subscribe((params) => {
+        const newArticleUrl = params.get('url');
+        this.articleUrl = newArticleUrl;
+        this.thearticle = null;
+        this.loadArticleDetails();
       });
     }
   }
 
-  loadArticleDetails(url: string): void {
-    this.articlesService.getArticleByUrlAsText(url).subscribe({
+  loadArticleDetails(): void {
+    if (!this.articleUrl) return;
+
+    this.articlesService.getArticleByUrlAsText(this.articleUrl).subscribe({
       next: (response) => {
-        this.article = response.data;
+        this.thearticle = response.data;
+        this.loadSomeArticles();
       },
       error: (err) => console.error('Error fetching article:', err),
     });
+  }
+
+  loadSomeArticles(): void {
+    if (!this.thearticle?.categoryId) return;
+
+    const paginationModel: PaginationModel = {
+      startRow: 0,
+      endRow: 3,
+    };
+
+    this.articlesService
+      .getPublishedArticlesByCategoryId(
+        this.thearticle.categoryId,
+        paginationModel
+      )
+      .subscribe({
+        next: (response: any) => {
+          this.someArticles = response.data;
+        },
+        error: (err) => console.error('Error fetching related articles:', err),
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.paramMapSubscription?.unsubscribe();
   }
 }
